a178dacdaa3ca5c879cb495c357c3ea47f393a9a
Jason <79180247+Bobsters6666@users.noreply.github.com>
2023-05-13 16:36:28 +1200

    vite1.1

--- gym-app/vite.config.js
+++ gym-app/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+import { defineConfig } from "file:///C:/Users/Jason/OneDrive%20-%20The%20University%20of%20Auckland/Code/Gym%20App/gym-app/node_modules/vite/dist/node/index.js";
+import react from "file:///C:/Users/Jason/OneDrive%20-%20The%20University%20of%20Auckland/Code/Gym%20App/gym-app/node_modules/@vitejs/plugin-react/dist/index.mjs";
+var vite_config_default = defineConfig({
+  plugins: [react()]
+});
+export {
+  vite_config_default as default
+};
